8066f4722db7356ad1ea65be5450b255375ad3d2
Richard Huynh <Richard Huynh>
2016-02-18 00:34:08 -0600

    created person and customer classes; first two bullet points of 11-1

---
+++ Project11_1/src/huynh/Person.java
@@ -0,0 +1,32 @@
+package huynh;
+
+/**
+ * Created by Richard Huynh on 2/17/2016.
+ */
+public abstract class Person {
+
+    // member fields
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    // no param constructor
+    public Person() {}
+
+    // get methods
+    public String getFirstName() { return this.firstName; }
+    public String getLastName() { return this.lastName; }
+    public String getEmail() { return this.email; }
+
+    // set methods
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setEmail(String email) { this.email = email; }
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s %s\nEmail: $s", this.firstName, this.lastName, this.email);
+    }
+
+    public abstract String getDisplayText();
+}
